99f5c8123793e6a0ab2e569f0424768a34a6ad4b
tkemaladze98 <tkemaladze.1998@gmail.com>
2022-05-06 19:24:26 +0400

    delete shota and mercedesBenz

--- meeting-2/homework-1/1-homework-solution.js
+++ meeting-2/homework-1/1-homework-solution.js
@@ -98,8 +98,7 @@ let daniel916 = new Person("Daniel", "Barbakadze", 21, "M", []);
 let ilona = new Person("Elon", "Musk", 30, "M");
 let duti_picoti = new Car("BMW", "525", "1999");
 let stodevianosto = new Car("Mercedes", "E190", 1991);
-let shota = new Person("shota", "tkemaladze", 23, "M", []);
-let mercedesBenz = new Car("Mercedes-benc", "C320", 1997);
+
 
 daniel916.buysCar(duti_picoti); // adds passed car
 daniel916.buysCar(stodevianosto); // adds passed car
